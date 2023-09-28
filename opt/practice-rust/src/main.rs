6704fc0d950f06a355d41de8ff0b9d30d153499a
oskimura <oskimura@gmail.com>
2023-09-28 21:09:42 +0900

    ecsからlightsailで動かすように方針転換

--- opt/practice-rust/src/main.rs
+++ opt/practice-rust/src/main.rs
@@ -23,7 +23,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new().service(todo_index)
     })
-        .bind("0.0.0.0:8080")?
+        .bind(("web","8080"))?
         .run()
         .await
 }
